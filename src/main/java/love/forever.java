58b8418bfa152d8ba93e41cb58fac2e5f210ee8f
ranjith <ranjithkumar143341@gmail.com>
2018-03-24 23:42:23 +0530

    night time

---
+++ src/main/java/love/forever.java
@@ -0,0 +1,7 @@
+package love;
+
+public class forever {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/love/heart.java
@@ -0,0 +1,14 @@
+package love;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class heart {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
